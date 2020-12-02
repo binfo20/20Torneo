e8f41d18b8cb61cc2c9c8596cd4af97ecf7b77fa
Tommaso Fachin <tommaso.fachin@gmail.com>
2020-12-02 15:36:57 +0100

    fatto fachin e medic

---
+++ Progetto2A/src/progetto2a/CreazioneCoppia.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progetto2a;
+
+/**
+ *
+ * @author Tommy
+ */
+public class CreazioneCoppia extends Giocatore{
+
+    public CreazioneCoppia(String nome, String cognome, int forza) {
+        super(nome, cognome, forza);
+    
+    }
+    
+    public class void CreaCoppia(){
+   
+        
+    }
+    
+    
+    
+}
